8e82434e9c3a293f7bbc762e199b9189142def6a
Sheila Hanson <slhanson11@live.com>
2024-03-14 21:40:42 -0500

    added webpack.config.js but have errors yet to fix

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,46 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     // Entry pont for files
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     // Output for bundles
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       // Webpack that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'J.A.T.E'
+        template: "./index.html",
+        title: "J.A.T.E",
       }),
       // Injects our custom service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
-      
+
       // Creates a manifest.json file
       new WebpackPwaManifest({
         inject: true,
-
-      })
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Takes notes while being in your browser",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/image/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
+      // CSS loader
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use. ['style-loader', 'css-loader'],
+        },
+        {test: /\.m?js$/,
+      exclude: /node_modules/,
+    // We use babel-loader in order to use ES6
+  use : {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@bable/preset-env'],
+      plugins: ['@bable/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+    },
+  },
+},
       ],
     },
   };
