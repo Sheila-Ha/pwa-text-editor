3d72ebb21e1c7693d45688ab9f08c4e195cc8795
Sheila Hanson <slhanson11@live.com>
2024-03-20 13:16:28 -0500

    Working through console errors and updating syntax in various files. Local Host is working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,8 +14,8 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("PUT to the database");
-  const jateDB = await openDB("jate, 1");
+  console.log("PUT to the database");
+  const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
   const request = store.put({ id: 1, value: content });
@@ -25,11 +25,11 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("GET from database");
-  const jateDB = await openDB("jate, 1");
+  console.log("GET from database");
+  const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log("Data saved to the db", result);
   return result?.value;
